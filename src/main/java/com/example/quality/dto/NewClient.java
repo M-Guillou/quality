63adaa1cea854bcfa96349ccc8b2a6f89f624ff2
Paco LLDP <pacolldp@gmail.com>
2025-06-12 10:44:07 +0200

    i hate jenkins

--- src/main/java/com/example/quality/dto/NewClient.java
+++ src/main/java/com/example/quality/dto/NewClient.java
@@ -2,10 +2,12 @@ package com.example.quality.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
+@Data
 public class NewClient {
     @NotBlank
     private String nom, prenom, adresse, codePostal, ville;
