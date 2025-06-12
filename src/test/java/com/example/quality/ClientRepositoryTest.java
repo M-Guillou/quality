3bf423edabef1eed4525ea2ca842042596566658
Paco LLDP <pacolldp@gmail.com>
2025-06-12 11:13:52 +0200

    test repo coverage

--- src/test/java/com/example/quality/ClientRepositoryTest.java
+++ src/test/java/com/example/quality/ClientRepositoryTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ClientRepositoryTest {
 
